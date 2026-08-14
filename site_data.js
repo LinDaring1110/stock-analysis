4698e945dd878a7c6a2c142988fcfa3bd02c0996
LinDaring1110 <71083711+LinDaring1110@users.noreply.github.com>
2026-08-14 15:05:30 +0800

    update site_data.js: refresh updates local snapshot (v2)

--- site_data.js
+++ site_data.js
@@ -2,27 +2,27 @@
 window.STATIC_INDICES = [
   {
     "name": "上证指数",
-    "price": 3908.93,
-    "changePct": -0.46
+    "price": 3927.18,
+    "changePct": 0.01
   },
   {
     "name": "沪深300",
-    "price": 4648.32,
-    "changePct": -0.34
+    "price": 4665.88,
+    "changePct": 0.04
   },
   {
     "name": "深证成指",
-    "price": 14264.79,
-    "changePct": -0.17
-  },
-  {
-    "name": "创业板指",
-    "price": 3602.24,
+    "price": 14354.31,
     "changePct": 0.45
   },
   {
+    "name": "创业板指",
+    "price": 3626.3,
+    "changePct": 1.12
+  },
+  {
     "name": "科创50",
-    "price": 1707.29,
-    "changePct": -0.61
+    "price": 1717.68,
+    "changePct": -0.0
   }
 ];
